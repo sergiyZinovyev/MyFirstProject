48d91c47b6d25f4c071c192efb6d4d6edff38100
sergiyZinovyev <Thrion1@gmail.com>
2019-02-13 12:28:35 +0200

    Add homework 7

--- homework/homework_7/JS_test_2/js/script.js
+++ homework/homework_7/JS_test_2/js/script.js
@@ -1,9 +1,21 @@
 function getSquare(x, y){
 	var res = new Array();
 	var item = 0;
-	for(var i=x; i<=y; i++){
-		res[item] = i*i;
+	x = Number(x);
+	y = Number(y);
+	for(var i=0; i<=(y-x); i++){
+		res[item] = (x+i)*(x+i);
 		item +=1;
 	}
-	alert (res);
+	return res;
 }
+function getFuncFor(){
+	//alert('test');
+	var inp1 = document.getElementById('input1_for').value;
+	//alert('inp1='+inp1);
+	var inp2 = document.getElementById('input2_for').value;
+	//alert('inp2='+inp2);
+	res = getSquare(inp1, inp2);
+	//alert('res='+res);
+	document.getElementById('output_for').value = res;
+	}
